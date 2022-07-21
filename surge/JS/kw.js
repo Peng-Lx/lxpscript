d209c4a0f651432fe029233150a651d2710b63c4
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-21 20:16:45 +0800

    Update kw.js

--- surge/JS/kw.js
+++ surge/JS/kw.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/PF2022/lxp/master/surge/JS/kw.js
 
 [mitm]
 hostname = vip1.kuwo.cn
@@ -14,7 +14,7 @@ hostname = vip1.kuwo.cn
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/PF2022/lxp/master/surge/JS/kw.js
 
 [MITM]
 hostname = vip1.kuwo.cn
