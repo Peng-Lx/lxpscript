d3dd1ffbedb29cf07df155fcc90623a26c1abbac
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-23 18:12:25 +0800

    Update Kuwo.js

--- JS/Kuwo.js
+++ JS/Kuwo.js
@@ -2,13 +2,13 @@
 酷我音乐 解锁会员试听及部分功能
 QX:
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/kuwo.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Kuwo.js
 ****************************
 hostname = vip1.kuwo.cn
 ****************************
 Surge4 or Loon:
 [Script]
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/kuwo.js
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Kuwo.js
 [MITM]
 */
 
