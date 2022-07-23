dac14134cbbb902ca538484d86f567d86d299078
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-23 14:10:34 +0800

    Update viva.js

--- JS/viva.js
+++ JS/viva.js
@@ -4,7 +4,7 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/viva-asia1\.vvbrd\.com\/api\/rest\/u\/vip* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/viva.js
+ ^https:\/\/viva-asia1\.vvbrd\.com\/api\/rest\/u\/vip* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/viva.js
  
  **********************
  *hostname = viva-asia1.vvbrd.com
@@ -12,7 +12,7 @@
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/viva-asia1\.vvbrd\.com\/api\/rest\/u\/vip* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/viva.js
+ http-response ^https:\/\/viva-asia1\.vvbrd\.com\/api\/rest\/u\/vip* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/viva.js
 */
  
  
