035c5c7548635a73dab898b4bd71880895211dc0
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 16:36:26 +0800

    Update endel.js

--- JS/endel.js
+++ JS/endel.js
@@ -1,18 +1,18 @@
 /*
- *脚本功能：endel
+ *脚本功能： 个性化音景 endel
  *脚本整理：Peng-Lx
        APP:https://apps.apple.com/us/app/endel-focus-sleep-relax/id1346247457?l=zh
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api-production\.endel\.io\/.*\/user$ url script-response-body 
+ ^https:\/\/api-production\.endel\.io\/.*\/user$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/endel.js
  **********************
  *hostname = api-production.endel.io
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api-production\.endel\.io\/.*\/user$ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api-production\.endel\.io\/.*\/user$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/endel.js
  
 */
  
