14c6461693b03119c67c5122025969a8449e0c22
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 15:06:15 +0800

    Update lingokids.js

--- JS/lingokids.js
+++ JS/lingokids.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api\.lingokids\.com\/v1\/renovate_session$ url script-response-body 
+ ^https:\/\/api\.lingokids\.com\/v1\/renovate_session$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/lingokids.js
  **********************
  *hostname = api.lingokids.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api\.lingokids\.com\/v1\/renovate_session$ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api\.lingokids\.com\/v1\/renovate_session$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/lingokids.js
  */
  
  
