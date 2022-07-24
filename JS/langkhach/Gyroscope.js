3b0f6740b86b9c21320fd8d29e6cbd44fb8b076d
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 12:31:05 +0800

    Update Gyroscope.js

--- JS/langkhach/Gyroscope.js
+++ JS/langkhach/Gyroscope.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body 
+ ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Gyroscope.js
  **********************
  *hostname = api.gyrosco.pe
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Gyroscope.js
  */
  
  
