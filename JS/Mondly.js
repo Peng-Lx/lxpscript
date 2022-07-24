653f581bc740f7bf45953d4fdfa203e32796e113
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 13:27:38 +0800

    Create Mondly.js

---
+++ JS/Mondly.js
@@ -0,0 +1,31 @@
+/*
+ *脚本功能：mondly
+ *脚本整理：Peng-Lx
+ **********************
+ *QX
+ [rewrite_local]
+ ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ url script-response-body 
+ **********************
+ *hostname =  api.mondlylanguages.com
+ **********************
+ 
+ *Surge&loon
+ [Script]
+ http-response ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ requires-body=1,max-size=0,script-path=
+ */
+ 
+ 
+ 
+var obj = JSON.parse($response.body);
+
+obj["pucharses"]= [10];
+obj["subscriptions"]=  {
+  "10": "2099-09-09 05:05:05"
+};
+obj["subscription"]=  {
+  "10": "2099-09-09 05:05:05"
+};
+obj["parsed_transactions"]= [
+  "540000370283138"
+];
+$done({body: JSON.stringify(obj)});
