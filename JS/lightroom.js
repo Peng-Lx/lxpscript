55cef41cecdba2409b36be827631b2559a14f1c8
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 14:19:27 +0800

    Update lightroom.js

--- JS/lightroom.js
+++ JS/lightroom.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/photos\.adobe\.io\/v2\/accounts* url script-response-body 
+ ^https:\/\/photos\.adobe\.io\/v2\/accounts* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/lightroom.js
  **********************
  *hostname =  photos.adobe.io
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/photos\.adobe\.io\/v2\/accounts* requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/photos\.adobe\.io\/v2\/accounts* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/lightroom.js
  */
  
  
