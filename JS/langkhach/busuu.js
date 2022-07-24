d893bcc257cf1587061e484bd1300fafe888c9ec
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 13:37:49 +0800

    Update busuu.js

--- JS/langkhach/busuu.js
+++ JS/langkhach/busuu.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api\.busuu\.com\/users\/me* url script-response-body 
+ ^https:\/\/api\.busuu\.com\/users\/me* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/busuu.js
  **********************
  *hostname =  api.busuu.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api\.busuu\.com\/users\/me* requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api\.busuu\.com\/users\/me* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/busuu.js
  */
  
  
