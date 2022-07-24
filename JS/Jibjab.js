50e6be2f6cde0b97af297452cd218940801e0a05
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 13:05:48 +0800

    Update Jibjab.js

--- JS/Jibjab.js
+++ JS/Jibjab.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user$ url script-response-body 
+ ^https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Jibjab.js
  **********************
  *hostname = api.memrise.com 
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user$) requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user$) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Jibjab.js
  */
  
  
