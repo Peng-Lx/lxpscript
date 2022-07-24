4eca1ce4eaa883398c687545e4a4ecfe70fd5094
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 10:35:52 +0800

    Update kuwo.js

--- quantumultx/JS/King/kuwo.js
+++ quantumultx/JS/King/kuwo.js
@@ -3,7 +3,7 @@
 脚本作者： By king
 [rewrite_local]
 # 解锁酷我音乐会员➕听书
-^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url script-response-body https://raw.githubusercontent.com/PF2022/lxp/master/quantumultx/JS/King/kuwo.js
+^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/quantumultx/JS/King/kuwo.js
 [mitm] 
 hostname = *.kuwo.cn
 ********************************
