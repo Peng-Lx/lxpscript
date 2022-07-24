9c727249ca4ac6759c7b1cc7f4233ff3b645c120
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 12:54:01 +0800

    Update Nhaccuatui.js

--- JS/langkhach/Nhaccuatui.js
+++ JS/langkhach/Nhaccuatui.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/graph\.nhaccuatui\.com\/.*\/users\/info* url script-response-body 
+ ^https:\/\/graph\.nhaccuatui\.com\/.*\/users\/info* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Nhaccuatui.js
  **********************
  *hostname = graph.nhaccuatui.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/graph\.nhaccuatui\.com\/.*\/users\/info* requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/graph\.nhaccuatui\.com\/.*\/users\/info* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Nhaccuatui.js
  */
  
  
