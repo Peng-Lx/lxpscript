9d66cec5823ab33ea9528c1e252770b1073520db
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 16:07:00 +0800

    Create Dayone.js

---
+++ JS/Dayone.js
@@ -0,0 +1,51 @@
+/*
+ *脚本功能：dayone
+ *脚本整理：Peng-Lx
+      APP:  https://apps.apple.com/us/app/day-one-%E6%97%A5%E8%AE%B0-%E7%AC%94%E8%AE%B0/id1044867788?l=zh
+      说明: Day One Premium
+            Day One 脚本由本脚本与 dayone-pre.js （以下称 pre 脚本）两部分组成，主脚本用于修改响应内容、pre 脚本用于屏蔽缓存
+           
+           1. 启用本脚本与 pre 脚本
+           2. 重启 Day One，稍等片刻等待高级版出现
+           3. 禁用掉 pre 脚本，重启 Day One，确认高级版状况不变*如果后续误操作导致 Day One 恢复为 Free/Plus 则重复上述操作。
+              
+		   4.脚本由 @ImSingee 与 @Mornwind 共同完成，请勿在公开群组、频道或是论坛、博客发布
+ **********************
+ *QX
+ [rewrite_local]
+ ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ url script-response-body 
+ **********************
+ *hostname = dayone.me
+ **********************
+ 
+ *Surge&loon
+ [Script]
+ http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ requires-body=1,max-size=0,script-path=
+ */
+ 
+ 
+ 
+ 
+ if ($response.statusCode == 200) {
+        if ($request.url.endsWith("account-status")) {
+
+            $done({body: JSON.stringify({
+  "expirationDate": 1893427199000,
+  "startDate": 1546272000000,
+  "subscriptionPlan": "com.bloombuilt.dayoneios.subscription.premium.yearly_discounted_trial",
+  "lastRenewalDate": 1546531200000,
+  "subscriptionName": "premium",
+  "bundleReason": "purchase",
+  "cancellationDate": 0
+} )});
+
+        } else {
+	     const bodyJson = JSON.parse($response.body);
+
+	     bodyJson.featureBundle = {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false}]};
+
+	    $done({body: JSON.stringify(bodyJson)});
+        }
+} else {
+	$done({});
+}
