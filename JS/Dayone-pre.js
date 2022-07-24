f2256c53892577b1968ee4e341c1314994ab46b8
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 16:24:05 +0800

    Update Dayone-pre.js

--- JS/Dayone-pre.js
+++ JS/Dayone-pre.js
@@ -6,14 +6,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/dayone\.me\/api\/users$ url script-response-body 
+ ^https:\/\/dayone\.me\/api\/users$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Dayone-pre.js
  **********************
  *hostname = dayone.me
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/dayone\.me\/api\/users$ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/dayone\.me\/api\/users$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Dayone-pre.js
  */
  
  
