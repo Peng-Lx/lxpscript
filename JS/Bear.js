a7f58440934999e8804f2c3ce890bffec34c54ce
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 11:37:51 +0800

    Update Bear.js

--- JS/Bear.js
+++ JS/Bear.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body 
+ ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Bear.js
  **********************
  *hostname = buy.itunes.apple.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Bear.js
  */
 
 var obj = JSON.parse($response.body);
