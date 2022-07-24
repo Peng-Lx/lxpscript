d537e851652ca46aa7da723b306ee8114a3cc108
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 13:34:08 +0800

    Update Mondly.js

--- JS/langkhach/Mondly.js
+++ JS/langkhach/Mondly.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ url script-response-body 
+ ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Mondly.js
  **********************
  *hostname =  api.mondlylanguages.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Mondly.js
  */
  
  
