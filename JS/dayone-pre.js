c9f9ab8c9549956e3e4d1c5212d58e9a77cfea3f
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 16:22:21 +0800

    Create dayone-pre.js

---
+++ JS/dayone-pre.js
@@ -0,0 +1,24 @@
+/*
+ *脚本功能：Byone_pre
+ *脚本整理：Peng-Lx
+      APP:  https://apps.apple.com/us/app/day-one-%E6%97%A5%E8%AE%B0-%E7%AC%94%E8%AE%B0/id1044867788?l=zh
+      说明: 本脚本是 dayone.js 的附属脚本，需与主脚本配合使用。具体使用方式请查看主脚本说
+ **********************
+ *QX
+ [rewrite_local]
+ ^https:\/\/dayone\.me\/api\/users$ url script-response-body 
+ **********************
+ *hostname = dayone.me
+ **********************
+ 
+ *Surge&loon
+ [Script]
+ http-response ^https:\/\/dayone\.me\/api\/users$ requires-body=1,max-size=0,script-path=
+ */
+ 
+ 
+ 
+ 
+ const headers = $request.headers
+delete headers["If-None-Match"]
+$done({headers})
