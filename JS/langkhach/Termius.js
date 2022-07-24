71164bbf34b084e5103a8cfec789511bdb0c3fd7
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 12:47:21 +0800

    Update Termius.js

--- JS/langkhach/Termius.js
+++ JS/langkhach/Termius.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body 
+ ^https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Termius.js
  **********************
  *hostname = api.termius.com
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/Termius.js
  */
  
  
