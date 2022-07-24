d6475c689fa546e293de21a89ed5705696e46c31
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-24 13:46:00 +0800

    Update elevate.js

--- JS/langkhach/elevate.js
+++ JS/langkhach/elevate.js
@@ -4,14 +4,14 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/accounts\.elevateapp\.net\/api\/users\?user%5Bauthentication_token* url script-response-body 
+ ^https:\/\/accounts\.elevateapp\.net\/api\/users\?user%5Bauthentication_token* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/elevate.js
  **********************
  *hostname =  accounts.elevateapp.net
  **********************
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/accounts\.elevateapp\.net\/api\/users\?user%5Bauthentication_token* requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/accounts\.elevateapp\.net\/api\/users\?user%5Bauthentication_token* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/langkhach/elevate.js
  */
  
  
