a0f467da4067369961c5ad2ba27c9169a019da49
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-26 09:06:36 +0800

    Create porhub.js

---
+++ surge/Script/ADB/porhub.js
@@ -0,0 +1,5 @@
+let ele = '<head>';
+let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let body = $response.body
+    .replace(ele, eleReplace)
+$done({ body });
