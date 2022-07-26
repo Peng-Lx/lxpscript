c36a357774545f11016e34a787f34bfad4c95409
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-26 09:09:12 +0800

    Create jableX.js

---
+++ surge/Script/ADB/jableX.js
@@ -0,0 +1,8 @@
+let rHead = '<head>'
+let newStyle = '<head><link rel=\"stylesheet\" href=\"https://limbopro.com/CSS/jableX.css\" type=\"text/css\">'
+var rBody = '</body>'
+var newJavaScript = '<script type=\"text/javascript\" async="async" src=\"//limbopro.com/Adguard/jableX.js\"></script></body>'
+let body = $response.body
+    .replace(rHead, newStyle)
+    .replace(rBody, newJavaScript);
+$done({ body });
