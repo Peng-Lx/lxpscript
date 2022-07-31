367a16e38e364828a5d073b0e8428f24a5948e92
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-07-31 17:56:19 +0800

    Update Wnds.js

--- quantumultx/JS/Nobyda/Wnds.js
+++ quantumultx/JS/Nobyda/Wnds.js
@@ -6,7 +6,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/quantumultx/JS/Nobyda/Wnds.js
 
 [mitm]
 hostname = p.du.163.com
@@ -15,7 +15,7 @@ hostname = p.du.163.com
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wnyd.js
+http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/quantumultx/JS/Nobyda/Wnds.js
 
 [MITM]
 hostname = p.du.163.com
