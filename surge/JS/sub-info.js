1feedc586a903e670af56fab44be6a9d0cb3e948
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-03 16:26:59 +0800

    Update sub-info.js

--- surge/JS/sub-info.js
+++ surge/JS/sub-info.js
@@ -12,7 +12,7 @@ Surge配置参考注释,感谢@congcong.
 AmyInfo = select, policy-path=http://sub.info?url=机场节点链接&reset_day=1&alert=1, update-interval=3600
 
 [Script]
-机场信息 = type=http-request,pattern=http://sub\.info,script-path=https://raw.githubusercontent.com/jnlaoshu/MySelf/master/Surge/Sub_info.js,timeout=10
+机场信息 = type=http-request,pattern=http://sub\.info,script-path= https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/surge/JS/sub-info.js,timeout=10
 ----------------------------------------
 
 脚本不用修改，直接配置就好。
