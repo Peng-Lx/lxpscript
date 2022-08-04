7d7b8e23eb8c1217bbf122becc38119872306921
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-04 11:41:59 +0800

    Create mryy.js

---
+++ JS/mryy.js
@@ -0,0 +1,27 @@
+/*
+ *脚本功能：每日英语阅读/每日外刊
+ *脚本整理：Peng-Lx
+      FROM：By Chamberlen
+ **********************
+ *QX
+ [rewrite_local]
+ ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url script-response-body 
+ **********************
+ *hostname = dict.eudic.net
+
+ **********************
+ 
+ *Surge&loon
+ [Script]
+ http-response ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url requires-body=1,max-size=0,script-path=
+ 
+ */
+ 
+ 
+
+let obj=JSON.parse($response.body);
+obj.tasks[0].finished= true;
+obj.tasks[0].task_action.user_purchase_status = "1";
+//obj.tasks[0].optional = true;
+//obj.tasks[0].finish_star = "1";
+$done({body:JSON.stringify(obj)})
