ad8c03b425ca96aabf760a44330dfa3ad234bfc9
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-04 11:43:14 +0800

    Update mryy.js

--- JS/mryy.js
+++ JS/mryy.js
@@ -5,7 +5,7 @@
  **********************
  *QX
  [rewrite_local]
- ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url script-response-body 
+ ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/mryy.js
  **********************
  *hostname = dict.eudic.net
 
@@ -13,7 +13,7 @@
  
  *Surge&loon
  [Script]
- http-response ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url requires-body=1,max-size=0,script-path=
+ http-response ^https:\/\/dict\.eudic\.net\/jingting\/GetThisChapterTaskStatus? url requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/mryy.js
  
  */
  
