5209977e562d69ca8271f0bb0090d497e6ca3614
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-12 20:36:38 +0800

    Update kw.js

--- surge/JS/kw.js
+++ surge/JS/kw.js
@@ -1,11 +1,12 @@
 /*
 酷我音乐 解锁会员试听及部分功能
+By Nobyda
 
 ***************************
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/PF2022/lxp/master/surge/JS/kw.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/surge/JS/kw.js
 
 [mitm]
 hostname = vip1.kuwo.cn
@@ -14,36 +15,4 @@ hostname = vip1.kuwo.cn
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/PF2022/lxp/master/surge/JS/kw.js
-
-[MITM]
-hostname = vip1.kuwo.cn
-
-**************************/
-
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/vip/v2/user/vip';
-const time = '/vip/spi/mservice';
-
-if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "2";
-	obj.data["vipLuxuryExpire"] = "1835312949000";
-	obj.data["time"] = "1961170340993";
-	obj.data["isYearUser"] = "2";
-	obj.data["vipmExpire"] = "1835312949000";
-	obj.data["vipOverSeasExpire"] = "1835312949000";
-	obj.data["vipExpire"] = "1835312949000";
-	obj.data["vip3Expire"] = "1835312949000";
-	body = JSON.stringify(obj);
-}
-
-if (url.indexOf(time) != -1) {
-	obj["isVIPMAutoPay"] = 2;
-	obj["isVIPLuxAutoPay"] = 2;
-	body = JSON.stringify(obj);
-}
-
-$done({body});
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/surge/JS/kw.js
