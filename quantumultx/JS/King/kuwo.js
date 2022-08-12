0c8fb47f5be0755ce4f3b938443567a6348cfb29
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-12 22:31:51 +0800

    Update kuwo.js

--- quantumultx/JS/King/kuwo.js
+++ quantumultx/JS/King/kuwo.js
@@ -7,6 +7,10 @@
 [mitm] 
 hostname = *.kuwo.cn
 ********************************
+
+*Surge&loon
+ [Script]
+ http-response ^http://(.+).kuwo.cn(/v2/api/user/info|/a.p|/vip/v2/user/vip) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Bear.js
 */
 
 
