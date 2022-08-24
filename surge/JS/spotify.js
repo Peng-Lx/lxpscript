6a827ff6d6be0c913c257641c3af8b82e58bfcde
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-24 20:40:39 +0800

    Update spotify.js

--- surge/JS/spotify.js
+++ surge/JS/spotify.js
@@ -7,7 +7,7 @@
  [rewrite_local]
  ^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/surge/JS/spotify.js
  **********************
- *hostname = pan.baidu.com
+ *hostname = spclient.wg.spotify.com
  **********************
  
  *Surge&loon
