59e59f548f57f18fefdcadac8f4630a5becd5900
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-08-27 22:09:28 +0800

    Update CamScanner.js

--- quantumultx/JS/Nobyda/CamScanner.js
+++ quantumultx/JS/Nobyda/CamScanner.js
@@ -5,7 +5,7 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body 
 
 [mitm]
 hostname = ap*.intsig.net
@@ -14,7 +14,7 @@ hostname = ap*.intsig.net
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=
 
 [MITM]
 hostname = ap*.intsig.net
