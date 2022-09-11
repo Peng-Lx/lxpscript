b1a549739228f9e74f3a2fa9bc2698669a500574
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-09-11 12:51:34 +0800

    Update timecard.js

--- surge/Panel/Script/timecard.js
+++ surge/Panel/Script/timecard.js
@@ -93,10 +93,10 @@ function icon_now(num) {
 }
 
 $done({
-  title: title_random(tnumcount(Number(nowlist))),
-  icon: icon_now(tnumcount(Number(nowlist))),
-  content: tlist[nowlist][0] + ":" + today(tnumcount(nowlist)) + "," + tlist[Number(nowlist) + Number(1)][0] + ":" + tnumcount(Number(nowlist) + Number(1)) + "天," + tlist[Number(nowlist) + Number(2)][0] + ":" + tnumcount(Number(nowlist) + Number(2)) + "天",
-  backgroundColor: '#FF3399'
+title:"节假日倒计时",
+icon:"list.dash.header.rectangle",
+'icon-color': "##FF3399",
+content: tlist[nowlist][0] + ":" + today(tnumcount(nowlist)) + "," + tlist[Number(nowlist) + Number(1)][0] + ":" + tnumcount(Number(nowlist) + Number(1)) + "天," + tlist[Number(nowlist) + Number(2)][0] + ":" + tnumcount(Number(nowlist) + Number(2)) + "天",
 })
 
 function title_random(num) {
