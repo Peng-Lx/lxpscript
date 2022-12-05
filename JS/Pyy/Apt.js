73a12b739a9a048983051129d61d8c320b15f422
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-06 04:03:23 +0800

    Update Apt.js

--- JS/Pyy/Apt.js
+++ JS/Pyy/Apt.js
@@ -3,11 +3,8 @@
 脚本功能：APTV-+解锁订阅
 下载地址：https://is.gd/inlPLD
 软件版本：1.0.16
-脚本作者：彭于晏💞
+脚本作者：彭于晏
 更新时间：2022-10-11
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
 TG频道群：https://t.me/py996
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
@@ -15,7 +12,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/APTV.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Pyy/Apt.js
 
 [mitm] 
 
