53d721add703f0c7e3b59aa4cd23af1784be4d35
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-06 16:06:25 +0800

    Create Nicegram.js

---
+++ JS/RE/Nicegram.js
@@ -0,0 +1,21 @@
+/*
+ 脚本功能：解锁Nicegram会员
+脚本作者：R·E
+支持版本：商店最新 1.0.7
+更新时间：2022.09.23
+问题反馈：https://t.me/Functional_Store_Hub
+使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，禁止转载与贩卖！⚠️⚠️⚠️
+ **********************
+ *QX
+ [rewrite_local]
+ ^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ url script-response-body 
+ **********************
+ *hostname = restore-access.indream.app
+ **********************
+ 
+ *Surge&loon
+ [Script]
+ http-response ^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ requires-body=1,max-size=0,script-path=
+ */
+
+const a=typeof $task!='undefined';const b=typeof $httpClient!='undefined';if(b){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'response':{'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'}});}else if(a){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':'HTTP/1.1\x20200\x20OK','headers':{'Content-Type':'application/json'},'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}else{console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}
