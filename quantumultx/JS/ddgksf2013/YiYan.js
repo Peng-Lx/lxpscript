aa78ddf4a710488b1e1ea9faa98a9e2959128155
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-07 22:34:39 +0800

    Create YiYan.js

---
+++ quantumultx/JS/ddgksf2013/YiYan.js
@@ -0,0 +1,24 @@
+/***
+ 脚本功能：一言(need登录)
+ 脚本作者：ddgksf2013
+ 更新时间：22.10.08
+ 使用声明：⚠️仅供学习交流，🈲️商业用途
+ 脚本致谢：感谢作者的半无私付出-Thanks👍
+ **********************
+ QX
+ [rewrite_local]
+ ^https:\/\/app\.yiyan\.art\/yiyan\/ url script-response-body url script-response-body
+ 
+ **********************
+ hostname = app.yiyan.art
+ 
+ **********************
+ 
+ Surge&loon
+ [Scrip]
+ http-response ^https:\/\/app\.yiyan\.art\/yiyan\/ requires-body=1,max-size=0,script-path=
+ 
+ ***/
+
+
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxf10d2=["\x76\x69\x70\x74\x79\x70\x65\x22\x3A\x22\x34\x22","\x72\x65\x70\x6C\x61\x63\x65","\x62\x6F\x64\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var body=$response[__Oxf10d2[0x2]][__Oxf10d2[0x1]](/viptype":"1"/g,__Oxf10d2[0x0]);$done({body});;;(function(_0x8530x2,_0x8530x3,_0x8530x4,_0x8530x5,_0x8530x6,_0x8530x7){_0x8530x7= __Oxf10d2[0x3];_0x8530x5= function(_0x8530x8){if( typeof alert!== _0x8530x7){alert(_0x8530x8)};if( typeof console!== _0x8530x7){console[__Oxf10d2[0x4]](_0x8530x8)}};_0x8530x4= function(_0x8530x9,_0x8530x2){return _0x8530x9+ _0x8530x2};_0x8530x6= _0x8530x4(__Oxf10d2[0x5],_0x8530x4(_0x8530x4(__Oxf10d2[0x6],__Oxf10d2[0x7]),__Oxf10d2[0x8]));try{_0x8530x2= __encode;if(!( typeof _0x8530x2!== _0x8530x7&& _0x8530x2=== _0x8530x4(__Oxf10d2[0x9],__Oxf10d2[0xa]))){_0x8530x5(_0x8530x6)}}catch(e){_0x8530x5(_0x8530x6)}})({})
