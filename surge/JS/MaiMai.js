4dae881082d6cd4a9e0f2719a01d749194cb7a15
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-07 23:23:54 +0800

    Create MaiMai.js

---
+++ surge/JS/MaiMai.js
@@ -0,0 +1,24 @@
+/***
+ 脚本功能：脉脉vip 
+ 脚本作者：66.66.66
+ 更新时间：66.66.66
+ 使用声明：⚠️仅供学习交流，🈲️商业用途
+ 脚本致谢：感谢作者的无私付出-Thanks👍
+ **********************
+ QX
+ [rewrite_local]
+ ^https:\//open.taou.com\/maimai\/member\/uh\/get_member_info_ios_v3 url script-response-body  script-response-body
+ 
+ **********************
+ hostname = open.taou.com
+ 
+ **********************
+ 
+ Surge&loon
+ [Scrip]
+ http-response ^https:\//open.taou.com\/maimai\/member\/uh\/get_member_info_ios_v3 requires-body=1,max-size=0,script-path=
+ 
+ ***/
+ var body = $response.body
+    .replace(/\"real_expire_time.*?activity\"/, "\"real_expire_time\": \"999\\u5929\\u540e\\u5230\\u671f\", \"real_expire_days\": 999, \"expire_time_list\": [{\"name\": \"\\u804c\\u573aVIP\\u4f1a\\u5458\", \"expire_time\": \"2099-12-31\\u5230\\u671f\"}]}, \"activity\"");
+$done({ body });
