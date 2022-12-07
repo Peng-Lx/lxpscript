9045119ff26b034714671d04bb7301461e63d10d
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-07 23:19:53 +0800

    Create 58infor.js

---
+++ surge/JS/58infor.js
@@ -0,0 +1,24 @@
+/***
+ 脚本功能：58同城 vip
+ 脚本作者：66.66.66
+ 更新时间：66.66.66
+ 使用声明：⚠️仅供学习交流，🈲️商业用途
+ 脚本致谢：感谢作者的无私付出-Thanks👍
+ **********************
+ QX
+ [rewrite_local]
+ ^https:\//passport.58.com\/58\/user/\info url script-response-body  script-response-body
+ 
+ **********************
+ hostname = passport.58.com
+ 
+ **********************
+ 
+ Surge&loon
+ [Scrip]
+ http-response ^https:\//passport.58.com\/58\/user/\info requires-body=1,max-size=0,script-path=
+ 
+ ***/
+var body = $response.body
+    .replace(/\"isVip\":false/, "\"isVip\":true");
+$done({ body });
