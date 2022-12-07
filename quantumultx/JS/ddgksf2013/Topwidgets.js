976b4b03448da4382be16105e0766824aba694a1
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-07 22:42:59 +0800

    Update Topwidgets.js

--- quantumultx/JS/ddgksf2013/Topwidgets.js
+++ quantumultx/JS/ddgksf2013/Topwidgets.js
@@ -6,7 +6,7 @@
  **********************
  QX
  [rewrite_local]
- ^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig url script-response-body
+ ^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/quantumultx/JS/ddgksf2013/Topwidgets.js
  
  **********************
  hostname = *xiaozujian.com
@@ -15,7 +15,7 @@
  
  Surge&loon
  [Scrip]
- http-response ^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig requires-body=1,max-size=0,script-path=
+ http-response ^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/quantumultx/JS/ddgksf2013/Topwidgets.js
  
  ***/
 ['so.js']["\x66\x69\x6c\x74\x65\x72"]["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"](((['']+[])["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"]['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65']['\x61\x70\x70\x6c\x79'](null,"36e100y111c110j101u40j123z98K111b100Z121S58u36z114N101L115x112A111d110b115K101f46A98p111R100A121R46P114w101x112e108u97w99F101Q40f47I115u117B98X115V99R114U105D98A101d34r58Z102c97R108W115X101X47y103a44T32g39t115Y117z98k115C99V114d105G98V101a34c58y116j114l117h101J39U41H125J41"['\x73\x70\x6c\x69\x74'](/[a-zA-Z]{1,}/))))('');
