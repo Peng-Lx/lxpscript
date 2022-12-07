11f88037546986e98564c75e1ec7beaf7b92a040
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2022-12-07 16:00:40 +0800

    Create TnAj.js

---
+++ JS/Crazy-Z7/TnAj.js
@@ -0,0 +1,21 @@
+
+/***
+ 脚本功能：铁牛爱经 Unlock Vip
+ 脚本作者：Crazy-Z7
+ 更新时间：66.66.66
+ 使用声明：⚠️仅供学习交流，🈲️商业用途
+ 脚本致谢：感谢作者的无私付出-Thanks👍
+ **********************
+ QX
+ [rewrite_local]
+ ^https?:\/\/admin\.tieniupro\.com\/rest\/getUserById url script-response-body
+ 
+ **********************
+ hostname = admin.tieniupro.com
+ **********************
+ 
+ Surge&loon
+ [Scrip]
+ http-response ^https?:\/\/admin\.tieniupro\.com\/rest\/getUserById requires-body=1,max-size=0,script-path=
+ 
+ ***/
