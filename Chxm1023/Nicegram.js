03135e1ca6e5f892fcc323551964a2015453bb4f
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2023-04-19 06:51:55 +0800

    Create Nicegram.js

---
+++ Chxm1023/Nicegram.js
@@ -0,0 +1,18 @@
+/*
+# Nicegram
+; hostname = restore-access.indream.app
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/chxm1023/Rewrite/main/Nicegram.js
+# 第二种方法，二选一即可
+#^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ url script-echo-response https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/Nicegram/Script/Nicegram.js
+
+*/
+
+
+
+
+
+{
+  "data" : {
+    "premiumAccess" : true
+  }
+}
