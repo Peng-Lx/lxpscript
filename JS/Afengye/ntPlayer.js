8b0cceecba001fa72799ee2ac900a645e76d2fa0
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2024-09-20 23:14:25 +0800

    Create ntPlayer.js

---
+++ JS/Afengye/ntPlayer.js
@@ -0,0 +1,41 @@
+/*******************************
+脚本功能：ntPlayer-解锁订阅
+脚本作者：afengye
+注意事项：开启脚本再进入软件，多尝试几次
+频道地址：https://t.me/afengye
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ntPlayer.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/ntPlayer.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
+
+let obj = {};
+
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+}else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  if(body && body.subscriber) {
+    const name = "\u0063\u006f\u006d\u002e\u006c\u006c\u002e\u0062\u0074\u0070\u006c\u0061\u0079\u0065\u0072\u002e\u0031\u0032";
+    const appid = "\u0063\u006f\u006d\u002e\u006c\u006c\u002e\u0062\u0074\u0070\u006c\u0061\u0079\u0065\u0072\u002e\u0031\u0032";
+    let data = {
+      "expires_date": "2999-01-01T00:00:00Z",
+      "original_purchase_date":  "2021-01-01T00:00:00Z",
+      "purchase_date": "2021-01-01T00:00:00Z",
+      "ownership_type": "PURCHASED",
+      "store": "app_store"
+    };
+    let subscriber = body.subscriber;
+    subscriber.subscriptions[(appid)] = data;
+    subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+    subscriber.entitlements[(name)].product_identifier = (appid);   
+    obj.body = JSON.stringify(body);
+  } 
+}
+
+$done(obj);
