7fafe79f480640dfa6e9727473fd7d572c1b3324
peng-Lx <98009929+Peng-Lx@users.noreply.github.com>
2024-09-20 23:15:23 +0800

    Create aptv.js

---
+++ JS/Afengye/aptv.js
@@ -0,0 +1,41 @@
+/*******************************
+脚本功能：APTV-解锁订阅
+脚本作者：afengye
+注意事项：开启脚本再进入软件
+频道地址：https://t.me/afengye
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/aptv.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/aptv.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
+
+let obj = {};
+
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+}else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  if(body && body.subscriber) {
+    const name = "pro";
+    const appid = "com.kimen.aptvpro.lifetime";
+    let data = {
+      "expires_date": "2999-01-01T00:00:00Z",
+      "original_purchase_date":  "2021-01-01T00:00:00Z",
+      "purchase_date": "2021-01-01T00:00:00Z",
+      "ownership_type": "PURCHASED",
+      "store": "app_store"
+    };
+    let subscriber = body.subscriber;
+    subscriber.subscriptions[(appid)] = data;
+    subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+    subscriber.entitlements[(name)].product_identifier = (appid);   
+    obj.body = JSON.stringify(body);
+  } 
+}
+
+$done(obj);
